e1fe35cd9fec40edeaed4c8d550ab36167918a71
Cédric Bouron <72381443+cedric16370@users.noreply.github.com>
2023-04-14 16:35:17 +0200

    First

---
+++ publisher.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <mqtt/async_client.h>
+
+using namespace std;
+
+const string ADDRESS { "tcp://broker.emqx.io:1883" }; // adresse du broker
+const string CLIENT_ID { "Cedric" }; // identifiant du client
+const string TOPIC { "/ynov/bordeaux/" }; // topic sur lequel publier le message
+const string PAYLOAD { "Cédric" }; // message à publier
+
+const int QOS = 0; 
+
+class callback : public virtual mqtt::callback // classe de callback pour traiter les messages reçus
+{
+public:
+    void connection_lost(const string& cause) override { // appelée lorsque la connexion est perdue
+        cout << "\nConnexion perdue" << endl;
+        if (!cause.empty()) {
+            cout << "\tCause : " << cause << endl;
+        }
+    }
+
+    void delivery_complete(mqtt::delivery_token_ptr token) override { // appelée lorsque le message est envoyé avec succès
+        cout << "\nMessage envoyé avec succès" << endl;
+    }
+
+    void message_arrived(const mqtt::const_message_ptr msg) override { // appelée lorsqu'un message est reçu
+        cout << "Message reçu : "
+            << msg->to_string() << endl; // affiche le message reçu
+    }
+};
+
+int main(int argc, char* argv[]) {
+
+    mqtt::async_client client(ADDRESS, CLIENT_ID); // création du client
+
+    callback cb; // création de l'objet de callback
+    client.set_callback(cb); // configuration du callback
+
+    mqtt::connect_options connOpts; // création des options de connexion
+    connOpts.set_clean_session(true); // définit une session propre
+
+    cout << "Connexion au broker '" << ADDRESS << "'..." << flush;
+    mqtt::token_ptr conntok = client.connect(connOpts); // connexion au broker
+    conntok->wait(); // attente de la connexion
+    cout << "Connecté !" << endl;
+
+    mqtt::token_ptr subtok = client.subscribe(TOPIC, QOS); // abonnement au topic
+    subtok->wait(); // attente de l'abonnement
+    cout << "Abonnement au topic '" << TOPIC << "'..." << flush;
+
+    mqtt::message_ptr pubmsg = mqtt::make_message(TOPIC, PAYLOAD); // création du message à publier
+    pubmsg->set_qos(QOS); // définition de la qualité de service
+    cout << "\nEnvoi du message '" << PAYLOAD << "' sur le topic '"
+        << TOPIC << "'..." << flush;
+    mqtt::token_ptr pubtok = client.publish(pubmsg); // publication du message
+    pubtok->wait(); // attente de la publication
+
+    client.disconnect(); // déconnexion du broker
+    cout << "Déconnexion du broker." << endl;
+
+    return 0;
+}
---
+++ subscriber.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <stdexcept>
+#include <mqtt/async_client.h>
+
+using namespace std;
+
+typedef mqtt::async_client mqtt_client_t; // Nouveau type pour mqtt::async_client
+typedef mqtt::connect_options mqtt_conn_opts_t; // Nouveau type pour mqtt::connect_options
+typedef mqtt::const_message_ptr mqtt_msg_ptr_t; // Nouveau type pour mqtt::const_message_ptr
+
+// Structure contenant les informations pour se connecter au broker MQTT
+struct st_mqtt_conn_info {
+    string server_address;
+    string client_id;
+    string topic;
+} mqtt_conn_info = {
+    "tcp://broker.emqx.io:1883",
+    "cedric-subscriber",
+    "/ynov/bordeaux/"
+};
+
+// Callback appelé lors de la réception d'un message sur le topic auquel on est abonné
+void on_message_received(mqtt_msg_ptr_t msg) {
+    cout << "Message reçu : " << msg->to_string() << endl;
+}
+
+int main(int argc, char* argv[]) {
+    // Initialisation du client MQTT
+    mqtt_client_t client(mqtt_conn_info.server_address, mqtt_conn_info.client_id);
+
+    // Options de connexion au broker
+    mqtt_conn_opts_t conn_opts;
+    conn_opts.set_keep_alive_interval(20);
+    conn_opts.set_clean_session(true);
+
+    try {
+        // Connexion au broker
+        cout << "Connexion au broker MQTT..." << endl;
+        mqtt_client_t::connect_options conn_options;
+        client.connect(conn_options)->wait();
+        cout << "Connecté au broker MQTT !" << endl;
+
+        // Abonnement au topic spécifié dans mqtt_conn_info
+        cout << "Abonnement au topic " << mqtt_conn_info.topic << "..." << endl;
+        client.subscribe(mqtt_conn_info.topic, 0)->wait();
+        cout << "Abonné au topic !" << endl;
+
+        // Boucle infinie pour recevoir les messages
+        while (true) {
+            // Attente d'un message
+            auto msg = client.consume_message();
+            if (msg) {
+                // Traitement du message reçu
+                on_message_received(msg);
+            }
+        }
+    }
+    catch (const mqtt::exception& exc) {
+        cerr << "Erreur : " << exc.what() << endl;
+        return 1;
+    }
+    return 0;
+}
